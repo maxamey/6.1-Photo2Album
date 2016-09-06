d142cb8512a0e2222f177b0e472f90e759d66210
Max Amey <maxwellamey@gmail.com>
2016-09-06 16:40:17 -0500

    Changed the structure of my object so that I can loop over it easier.

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 import React from 'react'
 import { render } from 'react-dom'
-import Main from './lib/main'
+import { Router, Route, hashHistory } from 'react-router'
+import Main from './lib/Main'
+import Nav from './lib/Nav'
+import Album from './lib/Album'
 
 render((
-  <Main name="Boilerplate"/>
+  <Router history={ hashHistory }>
+    <Route path="/"
+           component={ Main } />
+    <Route path="/nav"
+           component={ Nav }>
+      <Route path="/album"
+             compnent={ Album } />
+    </Route>
+  </Router>
 ), document.getElementById('app'))
